397cf71d5d76f778fdf938e4c5186b24d68cdb42
ayatbd <albehestione@gmail.com>
2023-11-07 23:45:38 +0600

    added biodata api

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-// const jwt = require("jsonwebtoken");
 
 // midlwares
 app.use(cors());
@@ -12,26 +12,26 @@ app.use(express.json());
 
 // ---------------------------
 
-// const verifyJWT = (req, res, next) => {
-//   const authorization = req.headers.authorization;
-//   if (!authorization) {
-//     return res
-//       .status(401)
-//       .send({ error: true, message: "unauthorized access" });
-//   }
-//   const token = authorization.split(" ")[1];
-//   5;
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  5;
 
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res
-//         .status(401)
-//         .send({ error: true, message: "unauthorized access" });
-//     }
-//     req.decoded = decoded;
-//     next();
-//   });
-// };
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 // ---------------------------
 
@@ -71,17 +71,18 @@ app.listen(port, () => {
 });
 
 const usersCollection = client.db("matWebDb").collection("users");
+const biodataCollection = client.db("matWebDb").collection("biodatas");
 
 // JWT TOKEN
 
-// app.post("/jwt", (req, res) => {
-//   const user = req.body;
-//   const token = jwt.sign(quser, process.env.ACCESS_TOKEN_SECRET, {
-//     expiresIn: "1h",
-//   });
+app.post("/jwt", (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(quser, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
 
-//   res.send({ token });
-// });
+  res.send({ token });
+});
 
 // ------------------------------------
 // varify jwt-------
@@ -103,5 +104,35 @@ app.post("/users", async (req, res) => {
   }
 
   const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
+
+// ----------------------------------
+// admin api
+
+app.get("/biodata", async (req, res) => {
+  const result = await biodataCollection.find().toArray();
+  res.send(result);
+});
+
+app.post("/biodata", async (req, res) => {
+  const newBiodata = req.body;
+  const result = await biodataCollection.insertOne(newBiodata);
+  res.send(result);
+});
+
+// ----------------------------------
+// admin api
+
+app.patch("/users/admin/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      role: "admin",
+    },
+  };
+
+  const result = await usersCollection.updateOne(filter, updateDoc);
   res.send(result);
 });
