ea41f23ad5d705f2f0ad764000e98ad40cc6230d
A Behesti <albehestione@gmail.com>
2025-08-27 14:48:24 +0600

    update the web serve

--- index.js
+++ index.js
@@ -156,6 +156,13 @@ app.delete("/biodata/:id", async (req, res) => {
   res.send(result);
 });
 
+app.get("/biodata/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await biodataCollection.findOne(query);
+  res.send(result);
+});
+
 // backend biodata approve api
 app.patch("/biodata/approve/:id", async (req, res) => {
   const id = req.params.id;
